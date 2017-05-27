0424b611006b26815a02dda6b1fa5992270b3edc
knguyenuit <knguyen.uit@gmail.com>
2017-05-27 15:07:51 +0700

    - add level

---
+++ Techincal/Assets/Scripts/Script_Nguyen/LevelManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+    public UIBaseLevel[] m_arrayLevel;
+    private int m_levelUnLockCount = 1;
+
+    public int LevelUnLockCount
+    {
+        get { return m_levelUnLockCount; }
+        set
+        {
+            m_levelUnLockCount = value;
+            PlayerPrefs.SetInt(strLevelUnLockCount, m_levelUnLockCount);
+        }
+    }
+    private const string strLevelUnLockCount = "unlockCount";
+    // Use this for initialization
+    void Awake()
+    {
+        SetupFullLock();
+        LevelUnLockCount = PlayerPrefs.GetInt(strLevelUnLockCount);
+    }
+
+    private void SetupFullLock()
+    {
+        for (int i = 0; i < m_arrayLevel.Length; i++)
+        {
+            m_arrayLevel[i].IsLock = true;
+        }
+    }
+    void OnEnable()
+    {
+        CheckLock();
+    }
+    [ContextMenu("test")]
+    public void CheckLock()
+    {
+        if (LevelUnLockCount < 1)
+        {
+            LevelUnLockCount = 1;
+        }
+        for (int i = 0; i < LevelUnLockCount; i++)
+        {
+            m_arrayLevel[i].IsLock = false;
+        }
+    }
+	
+}
---
+++ Techincal/Assets/Scripts/Script_Nguyen/UIBaseLevel.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIBaseLevel : MonoBehaviour {
+
+	// Use this for initializatio
+    public int m_idOfLevel;
+    public Text m_txtContent;
+    [Header("config")]
+    private bool m_isLock;
+    public Image m_imgLock;
+
+	    
+    public bool IsLock
+    {
+        get
+        {
+            return m_isLock;
+        }
+        set
+        {
+            m_isLock = value;
+            if (m_isLock && m_imgLock)
+            {
+                GetComponent<Button>().interactable = false;
+                m_imgLock.gameObject.SetActive(true);
+                //m_imgLock.sprite = SpriteManager.Instance.GetSpriteByTypeName(eSpriteName.LOCK);
+            }
+            else
+            {
+                GetComponent<Button>().interactable = true;
+                m_imgLock.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(OnClickHandle);
+        m_txtContent.text = "LEVEL " + m_idOfLevel.ToString();
+    }
+
+    private void OnClickHandle()
+    {
+        //GameController.Instance.SetupWithLevel(this.m_idOfLevel - 1);
+        //SpawnEnemy.Instance.m_ispawnedEnemy = false;
+        //ScreenManager.Instance.ShowScreenByType(eScreenType.GAMEPLAY);
+        //AudioManager.Instance.PlayOneShot(AudioType.UI_CLICK);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
