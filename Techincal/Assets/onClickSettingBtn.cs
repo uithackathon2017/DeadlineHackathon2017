9b7e0c90f05d4f3fc2b782039fef34fb620b88d0
Tỷ Tỷ <T T>
2017-05-27 23:23:42 +0700

    menu setting

---
+++ Techincal/Assets/onClickSettingBtn.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class onClickSettingBtn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public void clickSetting()
+    {
+        var panel = GameObject.Find("Canvas/Coregameplay/Setting_panel");
+        panel.SetActive(!panel.active);
+    }
+
+    public void clickAbout()
+    {
+        var panel = GameObject.Find("Canvas/Coregameplay/AboutInfor");
+        panel.SetActive(!panel.active);
+        var panel1 = GameObject.Find("Canvas/Coregameplay/Setting_panel");
+        panel1.SetActive(false);
+    }
+
+    public void clickResume()
+    {
+        var panel = GameObject.Find("Canvas/Coregameplay/Setting_panel");
+        panel.SetActive(false);
+    }
+
+    public void clickExit()
+    {
+        Application.Quit();
+    }
+	
+    public void clikBackinAboutInfor()
+    {
+        var panel = GameObject.Find("Canvas/Coregameplay/AboutInfor");
+        panel.SetActive(!panel.active);
+        var panel1 = GameObject.Find("Canvas/Coregameplay/Setting_panel");
+        panel1.SetActive(true);
+    }
+}
---
+++ Techincal/Assets/script_aboutinfor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class script_aboutinfor : MonoBehaviour {
+    
+	// Use this for initialization
+    
+	void Start () {
+		var panel = GameObject.Find("Canvas/Coregameplay/AboutInfor");
+        panel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
