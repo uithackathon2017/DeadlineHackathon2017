62a252f7289d6c64441757f46be663d8013fb4aa
Nguyễn Cao Cường <nguyencaocuong.1996@gmail.com>
2017-05-27 12:40:40 +0700

    add

---
+++ Techincal/Assets/Scripts/Script_Cuong/CubeSpawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawn : MonoBehaviour {
+    public GameObject cube;
+    public float timer;
+	// Use this for initialization
+	void Start () {
+        Instantiate(cube);
+        timer = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+		if(timer >= 5){
+            Instantiate(cube);
+            timer = 0;
+        }
+	}
+}
--- Techincal/Assets/Scripts/Script_Cuong/movecube.cs
+++ Techincal/Assets/Scripts/Script_Cuong/movecube.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class movecube : MonoBehaviour {
 	// Use this for initialization
     Vector3 newPos = new Vector3(0,0,0);
-    public float velocity = 50.0f;
+    Vector3 defaultPos = new Vector3();
+    public float velocity = 200.0f;
 	void Start () {
         newPos = this.transform.position;
+        defaultPos = newPos;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         newPos = transform.position;
         newPos.z -= velocity * Time.deltaTime;
-        newPos.x += (float) (Mathf.Sin(Time.time) * 50 * Time.deltaTime);
+        float newX = (float)(newPos.z - defaultPos.z) / 200 * 3.14f;
+        newPos.x = defaultPos.x + Mathf.Sin(newX) * 100;
         this.transform.position = newPos;
 	}
 }
