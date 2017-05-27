b6f0cf3d2bb07e105db35397b8221cd783300c50
khoathien1996 <khoathien1996@gmail.com>
2017-05-27 12:11:53 +0700

    MoveCube

---
+++ Techincal/Assets/Scripts/Scripts_Thien/MoveCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour {
+    Vector3 pos;
+    public float velocity = -30.0f;
+	// Use this for initialization
+	void Start () {
+        pos = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        pos.z += Time.deltaTime * velocity;
+        this.transform.position = pos;
+	}
+}
---
+++ Techincal/Assets/SpawnCube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCube : MonoBehaviour {
+    public GameObject obj_to_Spawn;
+    public float time_Spawn = 5.0f;
+    float timer;
+	// Use this for initialization
+	void Start () {
+        Instantiate(obj_to_Spawn);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if((timer += Time.deltaTime) >= time_Spawn)
+        {
+            Instantiate(obj_to_Spawn);
+            timer = 0;
+        }
+	}
+}
